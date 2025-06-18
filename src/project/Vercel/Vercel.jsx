8e538baa523ca5a4ce093507b02800316f1353a3
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-06-18 22:54:05 +0530

    adding search functionality

--- src/project/Vercel/Vercel.jsx
+++ src/project/Vercel/Vercel.jsx
@@ -5,6 +5,7 @@ import quant from "../../assets/logo.ico";
 import ssr from "../../assets/Asset 1.ico";
 import pixel from "../../assets/favicon.ico";
 import cloud from "../../assets/cloud.png";
+import tvs from "../../assets/tvs.ico";
 import { useState } from "react";
 
 function Vercel() {
@@ -51,21 +52,29 @@ function Vercel() {
       github: "WeatherApp",
       img: cloud,
     },
+    {
+      name: "fetch-tv-shows",
+      href: "fetch-tv-shows.vercel.app",
+      github: "Fetch-TVShows",
+      img: tvs,
+    },
   ];
 
   const [grid, setGrid] = useState(true);
+  const [search, setSearch] = useState("");
 
   return (
     <section
-      className="w-screen h-screen p-2"
+      className="w-screen h-screen p-2 bg-[#fffafa]"
       style={{ fontFamily: `"Geist", sans-serif` }}
     >
       <h1 className="text-4xl text-center font-bold">Deployed Projects</h1>
       <br />
-      <div className="w-full flex items-center gap-4 p-2 text-2xl">
+      <form className="w-full flex items-center gap-4 p-2 text-2xl">
         <input
           type="text"
           className="focus:outline-none px-2 py-1.5 text-end rounded border-1 border-black/30 text-xl grow"
+          onChange={(e) => setSearch(e.target.value)}
         />
         <i className="bx bx-sort-a-z border border-black/30 p-2 rounded" />
         <div className="flex items-center p-1 border border-black/30 rounded">
@@ -82,19 +91,79 @@ function Vercel() {
             onClick={() => setGrid(false)}
           />
         </div>
-      </div>
+        <div className="text-lg bg-black text-white py-1.5 px-3 rounded font-semibold cursor-pointer">
+          Add New
+        </div>
+      </form>
       {grid ? (
         <section className="grid grid-cols-3 gap-4 p-2">
-          {projects.map((project) => (
-            <div className="border-2 border-black/20 flex flex-col gap-4 p-4 rounded-lg">
-              <div className="flex justify-between">
+          {projects
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.name.toLowerCase().includes(search);
+            })
+            .map((project) => (
+              <div className="border-2 border-black/20 flex flex-col gap-4 p-4 rounded-lg">
+                <div className="flex justify-between">
+                  <div className="flex items-center gap-4">
+                    <img
+                      src={project.img}
+                      alt={project.name}
+                      className="size-9"
+                    />
+                    <div className="text-sm">
+                      <p className="font-bold">{project.name}</p>
+                      <a
+                        href={`https://${project.href}`}
+                        target="_blank"
+                        className="hover:underline text-black/80 line-clamp-1"
+                      >
+                        {project.href}
+                      </a>
+                    </div>
+                  </div>
+                  <div className="text-3xl text-black/70 flex items-center gap-2">
+                    <a
+                      href={`https://github.dev/SumedhaSinghRathor/${project.github}`}
+                      target="_blank"
+                    >
+                      <i className="bx bx-code-alt rounded-full" />
+                    </a>
+                    <i className="bx bx-dots-horizontal-rounded" />
+                  </div>
+                </div>
+                <a
+                  href={`https://github.com/SumedhaSinghRathor/${project.github}`}
+                  target="_blank"
+                >
+                  <div className="bg-gray-300 w-fit max-w-full font-semibold flex items-center gap-2 px-2 rounded-full">
+                    <i className="bx bxl-github text-xl" />{" "}
+                    <p className="line-clamp-1">
+                      SumedhaSinghRathor/{project.github}
+                    </p>
+                  </div>
+                </a>
+              </div>
+            ))}
+        </section>
+      ) : (
+        <section className="grid grid-cols-1 p-2">
+          {projects
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.name.toLowerCase().includes(search);
+            })
+            .map((project) => (
+              <div className="flex justify-between items-center p-2 border border-black/30 rounded">
                 <div className="flex items-center gap-4">
                   <img
                     src={project.img}
                     alt={project.name}
-                    className="size-9"
+                    className="size-12"
                   />
-                  <div className="text-sm">
+                  <div>
                     <p className="font-bold">{project.name}</p>
                     <a
                       href={`https://${project.href}`}
@@ -105,53 +174,17 @@ function Vercel() {
                     </a>
                   </div>
                 </div>
-                <div className="text-3xl text-black/70 flex items-center gap-2">
-                  <i className="bx bx-code-alt border border-black rounded-full" />
-                  <i className="bx bx-dots-horizontal-rounded" />
-                </div>
+                <a
+                  href={`https://github.com/SumedhaSinghRathor/${project.github}`}
+                  target="_blank"
+                >
+                  <div className="flex gap-1 bg-gray-300 px-2 rounded-full font-semibold">
+                    <i className="bx bxl-github text-2xl" />
+                    <p>SumedhaSinghRathor/{project.github}</p>
+                  </div>
+                </a>
               </div>
-              <a
-                href={`https://github.com/SumedhaSinghRathor/${project.github}`}
-                target="_blank"
-              >
-                <div className="bg-gray-300 w-fit max-w-full font-semibold flex items-center gap-2 px-2 rounded-full">
-                  <i className="bx bxl-github text-xl" />{" "}
-                  <p className="line-clamp-1">
-                    SumedhaSinghRathor/{project.github}
-                  </p>
-                </div>
-              </a>
-            </div>
-          ))}
-        </section>
-      ) : (
-        <section className="grid grid-cols-1 p-2">
-          {projects.map((project) => (
-            <div className="flex justify-between items-center p-2 border border-black/30 rounded">
-              <div className="flex items-center gap-4">
-                <img src={project.img} alt={project.name} className="size-12" />
-                <div>
-                  <p className="font-bold">{project.name}</p>
-                  <a
-                    href={`https://${project.href}`}
-                    target="_blank"
-                    className="hover:underline text-black/80 line-clamp-1"
-                  >
-                    {project.href}
-                  </a>
-                </div>
-              </div>
-              <a
-                href={`https://github.com/SumedhaSinghRathor/${project.github}`}
-                target="_blank"
-              >
-                <div className="flex gap-1 bg-gray-300 px-2 rounded-full font-semibold">
-                  <i className="bx bxl-github text-2xl" />
-                  <p>SumedhaSinghRathor/{project.github}</p>
-                </div>
-              </a>
-            </div>
-          ))}
+            ))}
         </section>
       )}
     </section>
