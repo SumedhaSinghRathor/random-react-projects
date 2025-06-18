caa1ff6b324f1cb9f2dca4f9edca71b876696511
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-06-18 20:59:32 +0530

    adding grid and list view

--- src/project/Vercel/Vercel.jsx
+++ src/project/Vercel/Vercel.jsx
@@ -5,6 +5,7 @@ import quant from "../../assets/logo.ico";
 import ssr from "../../assets/Asset 1.ico";
 import pixel from "../../assets/favicon.ico";
 import cloud from "../../assets/cloud.png";
+import { useState } from "react";
 
 function Vercel() {
   const projects = [
@@ -52,6 +53,8 @@ function Vercel() {
     },
   ];
 
+  const [grid, setGrid] = useState(true);
+
   return (
     <section
       className="w-screen h-screen p-2"
@@ -66,17 +69,68 @@ function Vercel() {
         />
         <i className="bx bx-sort-a-z border border-black/30 p-2 rounded" />
         <div className="flex items-center p-1 border border-black/30 rounded">
-          <i className="bx bxs-grid-alt bg-black/20 p-1 rounded" />
-          <i className="bx bx-list-ul p-1" />
+          <i
+            className={`bx bxs-grid-alt p-1 rounded transition-colors ${
+              grid ? "bg-black/20" : ""
+            }`}
+            onClick={() => setGrid(true)}
+          />
+          <i
+            className={`bx bx-list-ul p-1 rounded transition-colors ${
+              grid ? "" : "bg-black/20"
+            }`}
+            onClick={() => setGrid(false)}
+          />
         </div>
       </div>
-      <section className="grid grid-cols-3 gap-4 p-2">
-        {projects.map((project) => (
-          <div className="border-2 border-black/20 flex flex-col gap-4 p-4 rounded-lg">
-            <div className="flex justify-between">
+      {grid ? (
+        <section className="grid grid-cols-3 gap-4 p-2">
+          {projects.map((project) => (
+            <div className="border-2 border-black/20 flex flex-col gap-4 p-4 rounded-lg">
+              <div className="flex justify-between">
+                <div className="flex items-center gap-4">
+                  <img
+                    src={project.img}
+                    alt={project.name}
+                    className="size-9"
+                  />
+                  <div className="text-sm">
+                    <p className="font-bold">{project.name}</p>
+                    <a
+                      href={`https://${project.href}`}
+                      target="_blank"
+                      className="hover:underline text-black/80 line-clamp-1"
+                    >
+                      {project.href}
+                    </a>
+                  </div>
+                </div>
+                <div className="text-3xl text-black/70 flex items-center gap-2">
+                  <i className="bx bx-code-alt border border-black rounded-full" />
+                  <i className="bx bx-dots-horizontal-rounded" />
+                </div>
+              </div>
+              <a
+                href={`https://github.com/SumedhaSinghRathor/${project.github}`}
+                target="_blank"
+              >
+                <div className="bg-gray-300 w-fit max-w-full font-semibold flex items-center gap-2 px-2 rounded-full">
+                  <i className="bx bxl-github text-xl" />{" "}
+                  <p className="line-clamp-1">
+                    SumedhaSinghRathor/{project.github}
+                  </p>
+                </div>
+              </a>
+            </div>
+          ))}
+        </section>
+      ) : (
+        <section className="grid grid-cols-1 p-2">
+          {projects.map((project) => (
+            <div className="flex justify-between items-center p-2 border border-black/30 rounded">
               <div className="flex items-center gap-4">
-                <img src={project.img} alt={project.name} className="size-9" />
-                <div className="text-sm">
+                <img src={project.img} alt={project.name} className="size-12" />
+                <div>
                   <p className="font-bold">{project.name}</p>
                   <a
                     href={`https://${project.href}`}
@@ -87,20 +141,19 @@ function Vercel() {
                   </a>
                 </div>
               </div>
-              <div className="text-3xl text-black/70 flex gap-2">
-                <i className="bx bx-code-alt" />
-                <i className="bx bx-dots-horizontal-rounded" />
-              </div>
+              <a
+                href={`https://github.com/SumedhaSinghRathor/${project.github}`}
+                target="_blank"
+              >
+                <div className="flex gap-1 bg-gray-300 px-2 rounded-full font-semibold">
+                  <i className="bx bxl-github text-2xl" />
+                  <p>SumedhaSinghRathor/{project.github}</p>
+                </div>
+              </a>
             </div>
-            <div className="bg-gray-300 w-fit max-w-full font-semibold flex items-center gap-2 px-2 rounded-full">
-              <i className="bx bxl-github text-xl" />{" "}
-              <p className="line-clamp-1">
-                SumedhaSinghRathor/{project.github}
-              </p>
-            </div>
-          </div>
-        ))}
-      </section>
+          ))}
+        </section>
+      )}
     </section>
   );
 }
