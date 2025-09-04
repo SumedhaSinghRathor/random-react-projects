c8e823fadf0ae1c0c19b9c56ed0e7550b8b54dc7
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-09-04 18:19:08 +0530

    Change sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -35,20 +35,32 @@ function Sidebar() {
 
   return (
     <aside
-      className={`fixed w-3xs h-full flex flex-col gap-4 bg-blue-700 p-4 text-white rounded-r-2xl transition-transform ${
+      className={`absolute w-3xs h-full flex flex-col gap-4 bg-blue-700 p-4 text-white rounded-r-lg transition-transform shadow-2xl ${
         toggle ? "translate-x-0" : "-translate-x-64"
       }`}
     >
       <i
-        className="bx bx-menu text-5xl text-white size-12 rounded-r-lg bg-blue-700 fixed top-12 left-64 cursor-pointer"
+        className="bx bx-menu text-5xl text-white size-12 rounded-r-lg bg-blue-700 fixed top-12 left-64 cursor-pointer shadow-2xl "
         onClick={() => setToggle(!toggle)}
-      ></i>
-      <div className="flex gap-4 items-center">
-        <Link to="/" onClick={() => setToggle(false)}>
-          <img src={logo} alt="react logo" className="size-12 shrink-0" />
-        </Link>
-        <h1 className="text-lg font-bold leading-6  ">Mini React Projects</h1>
-      </div>
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="8"
+        height="64"
+        viewBox="0 0 8 64"
+        className="fixed left-64 top-10"
+      >
+        <path d="M0 64C0 59.5817 3.58172 56 8 56H0V64Z" fill="#1447E6" />
+        <path d="M0 0C0 4.41828 3.58172 8 8 8H0V0Z" fill="#1447E6" />
+      </svg>
+      <Link
+        to="/"
+        onClick={() => setToggle(false)}
+        className="flex gap-4 items-center"
+      >
+        <img src={logo} alt="react logo" className="size-12 shrink-0" />
+        <h1 className="text-lg font-bold leading-6">Mini React Projects</h1>
+      </Link>
       <ul className="h-full">
         {projects.map((project) => (
           <Link to={project.to}>
