765f3fa82fd8e5aeea2bfa5113f252ce77dbcee6
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-09-04 18:22:29 +0530

    Link to Portfolio

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,15 +1,18 @@
-import React from "react";
 import logo from "../assets/Asset 1.ico";
+import preview from "../assets/MY PORTFOLIO.png";
+import LinkTooltip from "./LinkTooltip";
 
 function Link() {
   return (
-    <a href="https://sumedhasinghrathor.vercel.app/" target="_blank">
-      <img
-        src={logo}
-        alt="logo"
-        className="fixed size-18 p-2 bg-black rounded-full right-5 bottom-5"
-      />
-    </a>
+    <LinkTooltip infoText={preview} className="z-100">
+      <a href="https://sumedhasinghrathor.vercel.app/" target="_blank">
+        <img
+          src={logo}
+          alt="logo"
+          className="fixed size-18 p-2 bg-black rounded-full right-5 bottom-5"
+        />
+      </a>
+    </LinkTooltip>
   );
 }
 
