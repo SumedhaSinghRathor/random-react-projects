6316f7134dc3c06271e61d97341a68556af4c81d
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-12-10 16:42:02 +0530

    update calculator display

--- src/project/Calculator/Display.jsx
+++ src/project/Calculator/Display.jsx
@@ -2,7 +2,9 @@ function Display({ value, expression }) {
   return (
     <div className="bg-[#D9D2C2] w-full aspect-3/2 rounded-lg flex flex-col justify-end items-end p-4">
       <div className="font-bold text-xl opacity-45">{expression}</div>
-      <div className="text-5xl">{value}</div>
+      <div className="text-5xl w-full text-end truncate text-ellipsis">
+        {value}
+      </div>
     </div>
   );
 }
