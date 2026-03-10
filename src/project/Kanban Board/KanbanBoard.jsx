e077fb91a0f5ba8e1f318db7859743bbb1728bfc
SumedhaSinghRathor <sumedharathor@gmail.com>
2026-03-10 14:10:07 +0530

    change Kanban Board content

--- src/project/Kanban Board/KanbanBoard.jsx
+++ src/project/Kanban Board/KanbanBoard.jsx
@@ -1,34 +1,11 @@
 import { useState } from "react";
+import { animes } from "./anime";
 
 function KanbanBoard() {
-  const [columns, setColumns] = useState({
-    notStarted: {
-      name: "Not Started",
-      items: [
-        { id: 1, content: "Market Research" },
-        { id: 2, content: "Write Projects" },
-      ],
-    },
-    inProgress: {
-      name: "In Progress",
-      items: [{ id: 3, content: "Design UI Mockups" }],
-    },
-    upToDate: {
-      name: "Up To Date",
-      items: [{ id: 4, content: "Content of Up To Date" }],
-    },
-    dropped: {
-      name: "Dropped",
-      items: [{ id: 5, content: "Content of Dropped" }],
-    },
-    done: {
-      name: "Done",
-      items: [{ id: 6, content: "Content of Done" }],
-    },
-  });
+  const [columns, setColumns] = useState(animes);
 
   const [newTask, setNewTask] = useState("");
-  const [activeColumn, setActiveColumn] = useState("notStarted");
+  const [activeColumn, setActiveColumn] = useState("interestedIn");
   const [draggedItem, setDraggedItem] = useState(null);
 
   const addNewTask = () => {
@@ -48,7 +25,7 @@ function KanbanBoard() {
     const updatedColumns = { ...columns };
 
     updatedColumns[columnId].items = updatedColumns[columnId].items.filter(
-      (item) => item.id !== taskId
+      (item) => item.id !== taskId,
     );
 
     setColumns(updatedColumns);
@@ -75,7 +52,7 @@ function KanbanBoard() {
 
     // remove from previous column
     updatedColumns[fromColumn].items = updatedColumns[fromColumn].items.filter(
-      (i) => i.id !== item.id
+      (i) => i.id !== item.id,
     );
 
     // add to new column
@@ -86,23 +63,23 @@ function KanbanBoard() {
   };
 
   const columnStyles = {
-    notStarted: {
+    finished: {
       background: "bg-[#E6E5E3] text-[#494846]",
       border: "border border-[#494846]",
     },
-    inProgress: {
+    inTheMiddleOf: {
       background: "bg-[#CBE3F7] text-[#264A72]",
       border: "border border-[#264A72]",
     },
-    upToDate: {
+    KeepUpWith: {
       background: "bg-[#F2E3B7] text-[#6D5A2A]",
       border: "border border-[#6D5A2A]",
     },
-    done: {
+    left: {
       background: "bg-[#D7E6DD] text-[#355C46]",
       border: "border border-[#355C46]",
     },
-    dropped: {
+    interestedIn: {
       background: "bg-[#F7D9D5] text-[#763F3C]",
       border: "border border-[#763F3C]",
     },
@@ -123,7 +100,7 @@ function KanbanBoard() {
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
           placeholder="Enter a new task"
-          className="grow px-2 py-1 border rounded outline-0"
+          className="grow px-2 py-1 border rounded outline-0 text-end"
           onKeyDown={(e) => e.key === "Enter" && addNewTask()}
         />
 
